9a2676f771f368203ea87b1d91cca9d158471b96
Orlando Vallejos <orlandovallejos@gmail.com>
2017-02-04 12:37:21 -0300

    Created modules to have separation of functionalities. Error handler added in services and controller.

--- js/app.js
+++ js/app.js
@@ -13,7 +13,11 @@
     'use strict';
 
     angular
-        .module('giphyasm', []);
+        .module('giphyasm', [
+            'giphyasm.constants',
+            'giphyasm.services',
+            'giphyasm.search'
+        ]);
 })();
 
 // APP CONSTANTS
@@ -22,8 +26,15 @@
     'use strict';
 
     angular
-        .module('giphyasm')
-        .constant('GLOBAL', {
+        .module('giphyasm.constants', []);
+})();
+
+(function () {
+    'use strict';
+
+    angular
+        .module('giphyasm.constants')
+        .constant('CONFIG', {
             'urlSearch': 'http://api.giphy.com/v1/gifs/search?',
             'urlTrending': 'http://api.giphy.com/v1/gifs/trending?',
             'apiKey': 'dc6zaTOxFJmzC',
@@ -39,11 +50,21 @@
     'use strict';
 
     angular
-        .module('giphyasm')
+        .module('giphyasm.services', [
+            'giphyasm.constants',
+            'giphyasm.common'
+        ]);
+})();
+
+(function () {
+    'use strict';
+
+    angular
+        .module('giphyasm.services')
         .factory('GiphyService', GiphyService);
 
-    GiphyService.$inject = ['$http', 'GLOBAL'];
-    function GiphyService($http, GLOBAL) {
+    GiphyService.$inject = ['$http', 'CONFIG', 'CommonService'];
+    function GiphyService($http, CONFIG, CommonService) {
         var service = {
             search: searchFunc,
             trending: trendingFunc
@@ -55,20 +76,33 @@
         function searchFunc(options) {
             /*
             q - search query term or phrase
+            lang - (optional) specify default country for regional content; format is 2-letter ISO 639-1 country code. See list of supported languages here
             limit - (optional) number of results to return, maximum 100. Default 25.
             offset - (optional) results offset, defaults to 0.
             rating - (optional) limit results to those rated (y,g, pg, pg-13 or r).
-            lang - (optional) specify default country for regional content; format is 2-letter ISO 639-1 country code. See list of supported languages here
             fmt - (optional) return results in html or json format (useful for viewing responses as GIFs to debug/test)
             */
 
             options = options || {};
-            var limit = options.limit || GLOBAL.limitSearch;
-            var url = GLOBAL.urlSearch + 'api_key=' + GLOBAL.apiKey + '&q=' + options.q + '&limit=' + limit + '&offset=' + options.offset
-                + '&rating=' + options.rating + '&lang=' + options.lang + '&fmt=' + options.fmt;
+            var limit = options.limit || CONFIG.limitSearch;
+            var url = CONFIG.urlSearch + 'api_key=' + CONFIG.apiKey + '&q=' + options.q + '&limit=' + limit + '&offset=' + options.offset;
+
+            if (options.rating) {
+                url = url.concat('&rating=', options.rating);
+            }
+            if (options.lang) {
+                url = url.concat('&lang=', options.lang);
+            }
+            if (options.fmt) {
+                url = url.concat('&fmt=', options.fmt);
+            }
 
             return $http
-                .get(url);
+                .get(url)
+                .then(function (response) {
+                    return response;
+                })
+                .catch(CommonService.errorHandler);
         }
 
         function trendingFunc(options) {
@@ -79,23 +113,62 @@
             fmt - (optional) return results in html or json format (useful for viewing responses as GIFs to debug/test)
             */
             options = options || {};
-            var limit = options.limit || GLOBAL.limitTrending;
-            var url = GLOBAL.urlTrending + 'api_key=' + GLOBAL.apiKey + '&limit=' + limit;
+            var limit = options.limit || CONFIG.limitTrending;
+            var url = CONFIG.urlTrending + 'api_key=' + CONFIG.apiKey + '&limit=' + limit;
 
-            if (options.offset)
+            if (options.offset) {
                 url = url.concat('&offset=', options.offset);
-
-            if (options.rating)
+            }
+            if (options.rating) {
                 url = url.concat('&rating=', options.rating);
-            if (options.fmt)
+            }
+            if (options.fmt) {
                 url = url.concat('&fmt=', options.fmt);
+            }
 
             return $http
-                .get(url);
+                .get(url)
+                .then(function (response) {
+                    return response;
+                })
+                .catch(CommonService.errorHandler);
         }
     }
 })();
 
+
+
+// APP SERVICES
+// ----------------------------------- 
+(function () {
+    'use strict';
+
+    angular
+        .module('giphyasm.common', [
+        ]);
+})();
+
+(function () {
+    'use strict';
+
+    angular
+        .module('giphyasm.common')
+        .factory('CommonService', CommonService);
+
+    CommonService.$inject = ['$log'];
+    function CommonService($log) {
+        var service = {
+            errorHandler: errorHandlerFunc
+        };
+
+        return service;
+
+        //Method definitions
+        function errorHandlerFunc(error) {
+            $log.error(error);
+        }
+    }
+})();
 
 // Main Controller
 // ----------------------------------- 
@@ -103,11 +176,21 @@
     'use strict';
 
     angular
-        .module('giphyasm')
+        .module('giphyasm.search', [
+            'giphyasm.constants',
+            'giphyasm.common'
+        ]);
+})();
+
+(function () {
+    'use strict';
+
+    angular
+        .module('giphyasm.search')
         .controller('MainController', MainController);
 
-    MainController.$inject = ['GiphyService'];
-    function MainController(GiphyService) {
+    MainController.$inject = ['GiphyService', 'CommonService'];
+    function MainController(GiphyService, CommonService) {
         var vm = this;
 
         //Variables
@@ -127,11 +210,11 @@
             GiphyService.trending()
                 .then(function (response) {
                     console.log(response);
-                });
+                })
+                .catch(CommonService.errorHandler);
         }
 
         //Functions
-
 
     }
 })();
